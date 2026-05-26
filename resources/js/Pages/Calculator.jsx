61b65515d874644f033a288ccf9f9c36e912ae49
Sierazeke <kristapsandis@inbox.lv>
2026-05-26 15:45:05 +0300

    The calculator can calculate, has an improved design and added features

--- resources/js/Pages/Calculator.jsx
+++ resources/js/Pages/Calculator.jsx
@@ -7,9 +7,23 @@ export default function Calculator({ user }) {
     const [result, setResult] = useState('');
 
     useEffect(() => {
+        try {
+            setResult(eval(expression));
+        } catch {
+            // 
+        }
+    }, [expression]);
 
-    });
 
+    function CalcButton({ label, onClick }) {
+        return (
+            <button 
+                onClick={onClick}
+                className="bg-zinc-500 text-white w-20 h-14 mr-1 mt-1 rounded-lg hover:bg-zinc-600">
+                {label}
+            </button>
+        );
+    }
     return (
 
     <div className="bg-gray-300 min-h-screen">
@@ -43,35 +57,45 @@ export default function Calculator({ user }) {
 
     <div className="m-4">
 
-        <div className="p-2 bg-gray-400 w-1/4">{expression || '0'}</div>
+        <div className="p-2 bg-gray-400 w-1/3">{expression || '0'}</div>
 
-        <div className="text-2xl mt-2 bg-zinc-400 w-1/3 p-1 text-center">
-            <button onClick={() => setExpression(expression + '7')} className="mx-10">7</button>
-            <button onClick={() => setExpression(expression + '8')} className="mx-10">8</button>
-            <button onClick={() => setExpression(expression + '9')} className="mx-10">9</button>
-            <button onClick={() => setExpression(expression + '/')} className="mx-10">/</button>
+        <div className="p-2 bg-gray-400 w-1/3">= {result}</div>
+
+        <div>
+            <CalcButton label="7" onClick={() => setExpression(expression + '7')}/>
+            <CalcButton label="8" onClick={() => setExpression(expression + '8')}/>
+            <CalcButton label="9" onClick={() => setExpression(expression + '9')}/>
+            <CalcButton label="/" onClick={() => setExpression(expression + '/')}/>
         </div>
-        <div className="text-2xl mt-1 bg-zinc-400 w-1/3 p-1 text-center">
-            <button onClick={() => setExpression(expression + '4')} className="mx-10">4</button>
-            <button onClick={() => setExpression(expression + '5')} className="mx-10">5</button>
-            <button onClick={() => setExpression(expression + '6')} className="mx-10">6</button>
-            <button onClick={() => setExpression(expression + '*')} className="mx-10">*</button>
+
+        <div>
+            <CalcButton label="4" onClick={() => setExpression(expression + '4')}/>
+            <CalcButton label="5" onClick={() => setExpression(expression + '5')}/>
+            <CalcButton label="6" onClick={() => setExpression(expression + '6')}/>
+            <CalcButton label="*" onClick={() => setExpression(expression + '*')}/>
         </div>
-        <div className="text-2xl mt-1 bg-zinc-400 w-1/3 p-1 text-center">
-            <button onClick={() => setExpression(expression + '1')} className="mx-10">1</button>
-            <button onClick={() => setExpression(expression + '2')} className="mx-10">2</button>
-            <button onClick={() => setExpression(expression + '3')} className="mx-10">3</button>
-            <button onClick={() => setExpression(expression + '-')} className="mx-10">-</button>
+
+        <div>
+            <CalcButton label="1" onClick={() => setExpression(expression + '1')}/>
+            <CalcButton label="2" onClick={() => setExpression(expression + '2')}/>
+            <CalcButton label="3" onClick={() => setExpression(expression + '3')}/>
+            <CalcButton label="-" onClick={() => setExpression(expression + '-')}/>
         </div>
-        <div className="text-2xl mt-1 bg-zinc-400 w-1/3 p-1 text-center">
-            <button onClick={() => setExpression(expression + '0')} className="mx-10">0</button>
-            <button onClick={() => setExpression(expression + '(')} className="mx-10">(</button>
-            <button onClick={() => setExpression(expression + '+')} className="mx-10">+</button>
+
+        <div>
+            <CalcButton label="0" onClick={() => setExpression(expression + '0')}/>
+            <CalcButton label="(" onClick={() => setExpression(expression + '(')}/>
+            <CalcButton label="+" onClick={() => setExpression(expression + '+')}/>
+            <CalcButton label=")" onClick={() => setExpression(expression + ')')}/>
         </div>
-        <div className="text-2xl mt-1 bg-zinc-400 w-1/3 p-1 text-center">
-            <button onClick={() => setExpression('')} className="mx-10">C</button>
-            <button onClick={() => setExpression(expression + ')')} className="mx-10">)</button>
-            <button onClick={() => setResult('expression')} className="mx-10">=</button>
+
+        
+        <div>
+            <CalcButton label="C" onClick={() => setExpression('')}/>
+            <CalcButton label="=" onClick={() => setExpression(result)}/>
+            <CalcButton label="." onClick={() => setExpression(expression + '.')}/>
+            <CalcButton label="^" onClick={() => setExpression(expression + '**')}/>
+
         </div>
 
         <div className="mt-1 bg-zinc-400">
